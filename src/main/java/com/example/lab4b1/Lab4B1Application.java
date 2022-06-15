ceead7c1ad07fc0c0970b30bd55cd97604053eb7
truongnqfaderless <ntruong9964@gmail.com>
2022-06-15 08:54:27 +0700

    add all by truongnq

---
+++ src/main/java/com/example/lab4b1/Lab4B1Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4b1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4B1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4B1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab4b1/Lab4B1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4b1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4B1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
